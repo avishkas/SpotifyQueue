f3186daec32d95eb11f300638827f91ff084523e
Avishka Suduwa Dewage <avishkas5@gmail.com>
2018-01-01 17:25:34 -0600

    removed sensitive data

--- server.py
+++ server.py
@@ -5,8 +5,8 @@ import requests
 import dbManager
 app = Flask(__name__)
 
-clientID = "e30c552da1264f94a0414906a89b6eb8"
-clientSecret = "b12914ccec2449258eb4e5f23f7e3e67"
+clientID = ""
+clientSecret = ""
 clientCodeRelations = {}
 
 @app.route('/')
