9ad45c979340c1ac8b267987142f84617b97236e
unknown <avishkas5@gmail.com>
2018-01-01 22:41:49 -0600

    fixed minor bugs, found CORS issue

--- static/script.js
+++ static/script.js
@@ -1,7 +1,10 @@
+console.log("script loaded");
+
 $(document).ready(function(){
 	$('#login').on('click', function(){
+		console.log("login pressed");
 		$.post("/hostLogin", function(data){
-
+			console.log("hostlogin called");
 		});
 	})
 });
